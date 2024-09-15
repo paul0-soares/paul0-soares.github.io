592639d1f75ea7474d75ff57dc6b12178d6609db
Paulo Soares <soarespaullo@riseup.net>
2024-09-15 00:04:53 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,39 @@
 // Scripts
 // 
 
+//Coloca o botão em uma varivel
+var btn_subir = $(".botao-voltar-ao-topo");
+
+
+//Faz a primeira verificacao ao carregar a pagina
+$(document).ready(function(){
+    var minhaposicao = $(this).scrollTop();
+    if(minhaposicao >=100){
+        btn_subir.fadeIn();
+    }
+    else{
+        btn_subir.fadeOut();
+    }
+});
+
+//Fica monitorando a rolagem de pagina
+$(window).scroll(function(){
+    var minhaposicao = $(this).scrollTop();
+    
+    if(minhaposicao >=100){
+        btn_subir.fadeIn();
+    }
+    else{
+        btn_subir.fadeOut();
+    }
+});
+
+btn_subir.click(function(){
+    $('html,body').animate({scrollTop:0},500);
+})
+
+
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
