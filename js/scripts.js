66720c16f6bf18526aec4dc88578614e1c2560fd
Paulo Soares <soarespaullo@riseup.net>
2024-09-20 18:47:20 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,26 +6,6 @@
 //
 // Scripts
 // 
-   // Scroll to top button appear
-    document.addEventListener('scroll', () => {
-        const scrollToTop = document.body.querySelector('.scroll-to-top');
-        if (document.documentElement.scrollTop > 100) {
-            if (!scrollToTopVisible) {
-                fadeIn(scrollToTop);
-                scrollToTopVisible = true;
-            }
-        } else {
-            if (scrollToTopVisible) {
-                fadeOut(scrollToTop);
-                scrollToTopVisible = false;
-            }
-        }
-    })
-})
-
-
-
-
 
 window.addEventListener('DOMContentLoaded', event => {
 
